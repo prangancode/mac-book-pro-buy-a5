8bf789b89299e67d7590223be035b81ed229964b
prangancode <senprangan@gmail.com>
2021-08-21 21:12:55 +0600

    reduce line

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-// Memory getting Element By Id
-const memory8Gb = document.getElementById("8gb-memory");
-const memory16Gb = document.getElementById("16gb-memory");
+// // Memory getting Element By Id
+// const memory8Gb = document.getElementById("8gb-memory");
+// const memory16Gb = document.getElementById("16gb-memory");
 
 const extraMemoryCost = document.getElementById("extra-memory-cost");
 
-// Storage getting Element By Id
+// // Storage getting Element By Id
 
-const ssd256Gb = document.getElementById("256gb-ssd");
-const ssd512Gb = document.getElementById("512gb-ssd");
-const ssd1Tb = document.getElementById("1tb-ssd");
+// const ssd256Gb = document.getElementById("256gb-ssd");
+// const ssd512Gb = document.getElementById("512gb-ssd");
+// const ssd1Tb = document.getElementById("1tb-ssd");
 
 const extraStorageCost = document.getElementById("extra-storage-cost");
 
@@ -35,37 +35,37 @@ const total = document.getElementById("total");
 
 // Memory Onclick
 
-memory8Gb.addEventListener('click', function () {
+document.getElementById("8gb-memory").addEventListener('click', function () {
     extraMemoryCost.innerText = '0';
     updateTotal();
 });
-memory16Gb.addEventListener('click', function () {
+document.getElementById("16gb-memory").addEventListener('click', function () {
     extraMemoryCost.innerText = '180';
     updateTotal();
 });
 
 // Storage Onclick
 
-ssd256Gb.addEventListener('click', function () {
+document.getElementById("256gb-ssd").addEventListener('click', function () {
     extraStorageCost.innerText = '0';
     updateTotal();
 });
-ssd512Gb.addEventListener('click', function () {
+document.getElementById("512gb-ssd").addEventListener('click', function () {
     extraStorageCost.innerText = '100';
     updateTotal();
 });
-ssd1Tb.addEventListener('click', function () {
+document.getElementById("1tb-ssd").addEventListener('click', function () {
     extraStorageCost.innerText = '180';
     updateTotal();
 });
 
 // Choose your delivery option onclick
 
-freeCharge.addEventListener('click', function () {
+document.getElementById("free-charge").addEventListener('click', function () {
     deliveryChargeDisplay.innerText = '0';
     updateTotal();
 });
-deliveryCharge.addEventListener('click', function () {
+document.getElementById("delivery-charge").addEventListener('click', function () {
     deliveryChargeDisplay.innerText = '20';
     updateTotal();
 });
